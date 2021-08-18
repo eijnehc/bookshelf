66fd5dc650137b18d1233a4d6e3031af870c9c39
Chen Jie <chenjie@Chens-MacBook-Pro.local>
2021-08-19 03:47:31 +0800

    Complete Render a React App extra1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,23 +1,54 @@
 // 🐨 you'll need to import React and ReactDOM up here
 
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
+import {Dialog} from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
 import {Logo} from './components/logo'
 
+const STATUS = {
+  NONE: 'none',
+  LOGIN: 'login',
+  REGISTER: 'register',
+}
+
 function App() {
-  const handleLogin = () => alert('login clicked')
-  const handleRegister = () => alert('register clicked')
+  const [openModal, setOpenModal] = useState(STATUS.NONE)
+
+  const close = () => setOpenModal(STATUS.NONE)
 
   return (
     <>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={handleLogin}>Login</button>
+        <button onClick={() => setOpenModal(STATUS.LOGIN)}>Login</button>
+
+        <Dialog
+          isOpen={openModal === STATUS.LOGIN}
+          onDismiss={close}
+          aria-label="Login form"
+        >
+          <div>
+            <button onClick={close}>Close</button>
+          </div>
+          <h3>Login</h3>
+        </Dialog>
       </div>
       <div>
-        <button onClick={handleRegister}>Register</button>
+        <button onClick={() => setOpenModal(STATUS.REGISTER)}>Register</button>
+
+        <Dialog
+          isOpen={openModal === STATUS.REGISTER}
+          onDismiss={close}
+          aria-label="Register form"
+        >
+          <div>
+            <button onClick={close}>Close</button>
+          </div>
+          <h3>Register</h3>
+        </Dialog>
       </div>
     </>
   )
