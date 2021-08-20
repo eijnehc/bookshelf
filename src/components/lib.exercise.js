f3697190265553ed60a59892ff1018cb6e9c851f
Chen Jie <chenjie@Chens-MacBook-Pro.local>
2021-08-20 08:19:45 +0800

    Complete Add Styles ex 3

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled/macro'
 import {Dialog as ReachDialog} from '@reach/dialog'
+import {FaSpinner} from 'react-icons/fa'
+import {keyframes} from '@emotion/core'
 
 import * as mq from 'styles/media-queries'
 import * as colours from 'styles/colors'
@@ -54,4 +56,17 @@ const Dialog = styled(ReachDialog)({
   },
 })
 
-export {CircleButton, Dialog, Button, Input, FormGroup}
+const spin = keyframes({
+  '0%': {transform: 'rotate(0deg)'},
+  '100%': {transform: 'rotate(360deg)'},
+})
+
+const Spinner = styled(FaSpinner)({
+  animation: `${spin} 1s linear infinite`,
+})
+
+Spinner.defaultProps = {
+  'aria-label': 'loading',
+}
+
+export {CircleButton, Dialog, Button, Input, FormGroup, Spinner}
